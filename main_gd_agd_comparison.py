ab11115d5022f37e3ba8b4c500c51bfe9cd041a0
b3rt01ac3 <andre@bertolace.com>
2023-05-31 09:44:45 +0200

    adding new charts for the comparison between GD and AGD

--- main_gd_agd_comparison.py
+++ main_gd_agd_comparison.py
@@ -15,6 +15,7 @@ import time
 # fixing seed
 np.random.seed(1717171)
 
+
 def get_random_Q_p(k, size, integer):
     S = ortho_group.rvs(size)
     R = np.random.randint(1, 100, size) if integer else np.random.rand(size)
@@ -25,14 +26,15 @@ def get_random_Q_p(k, size, integer):
     p = np.random.rand(size, 1)
     return Q, p
 
+
 def get_random_Q_p_test(k, size):
     A = np.random.randn(size, size)
     B = A.T.dot(A)
-    n_zeros = np.random.randint(0, size*(size-1)/2+1)
+    n_zeros = np.random.randint(0, size * (size - 1) / 2 + 1)
     for _ in range(n_zeros):
         i = np.random.randint(1, size)
-        j = np.random.randint(0, size-1)
-        B[i, j] = 0.0 
+        j = np.random.randint(0, size - 1)
+        B[i, j] = 0.0
     R, S = np.linalg.eig(B)
     Rmin = np.min(R)
     Rmax = np.max(R)
@@ -41,28 +43,32 @@ def get_random_Q_p_test(k, size):
     p = np.random.rand(size, 1)
     return Q, p
 
+
 def f(x):
     return 0.5 * x.T.dot(Q).dot(x) + p.T.dot(x)
+
 
 def df(x):
     return Q.dot(x) + p
 
-def norm2(x): 
-    return np.sum(x**2)**0.5
+
+def norm2(x):
+    return np.sum(x ** 2) ** 0.5
+
 
 # Steps
 T_gd = 9
 T_agd = 6
 
 # Matrix params
-ds = [2,4,8]
+ds = [2, 4, 8]
 ks = [1.5, 2.0, 3.0, 5.0, 10.0, 20.0, 50.0]
 N = 1
 r = 0.5
 
 gd_last_step = []
-agd_last_step = [] 
-opt = [] 
+agd_last_step = []
+opt = []
 
 for sample in range(N):
     t0 = time.time()
@@ -74,7 +80,7 @@ for sample in range(N):
         for i, k in enumerate(ks):
             Q, p = get_random_Q_p(k, d, False)
             x_opt = -np.linalg.inv(Q).dot(p)
-            r_i = np.random.randn(d,1)
+            r_i = np.random.randn(d, 1)
             x0 = x_opt + r_i * r / norm2(r_i)
             f_opt = f(x_opt)[0][0]
             opt_i[i][j] = f_opt
@@ -127,8 +133,8 @@ for sample in range(N):
             xs_gd = gd_qp(Q, p, T_gd, x0)
             xs_gd_enc = he_gd_qp_ckks(Q_enc, p_enc, d, alpha, beta, T_gd, x0_enc, evaluator,
                                       ckks_encoder, gal_keys, relin_keys, encryptor, scale)
-            xs_gd_dec = [decrypt_array(val, decryptor, ckks_encoder, d, d)[:,0].reshape(d,1) for val in xs_gd_enc]
-            x_gd_dec_last = xs_gd_dec[len(xs_gd_dec)-1]
+            xs_gd_dec = [decrypt_array(val, decryptor, ckks_encoder, d, d)[:, 0].reshape(d, 1) for val in xs_gd_enc]
+            x_gd_dec_last = xs_gd_dec[len(xs_gd_dec) - 1]
             gd_last_step_i[i][j] = f(x_gd_dec_last)[0][0]
 
             # Problem Inputs HE GD
@@ -140,26 +146,82 @@ for sample in range(N):
             xs_agd = agd_qp(Q, p, T_agd, x0)
             xs_agd_enc = he_agd_qp_ckks(Q_enc, p_enc, d, alpha, beta, T_agd, x0_enc, y0_enc, evaluator,
                                         ckks_encoder, gal_keys, relin_keys, encryptor, scale)
-            xs_agd_dec = [decrypt_array(val, decryptor, ckks_encoder, d, d)[:,0].reshape(d,1) for val in xs_agd_enc]
-            x_agd_dec_last = xs_agd_dec[len(xs_agd_dec)-1]
+            xs_agd_dec = [decrypt_array(val, decryptor, ckks_encoder, d, d)[:, 0].reshape(d, 1) for val in xs_agd_enc]
+            x_agd_dec_last = xs_agd_dec[len(xs_agd_dec) - 1]
             agd_last_step_i[i][j] = f(x_agd_dec_last)[0][0]
     df_gd_last_step_i = pd.DataFrame(gd_last_step_i, columns=ds, index=ks)
-    df_gd_last_step_i.to_csv("./data/comparison/gd"+str(sample)+".csv")
+    df_gd_last_step_i.to_csv("./data/comparison/gd" + str(sample) + ".csv")
     gd_last_step += [df_gd_last_step_i]
-    df_agd_last_step_i = pd.DataFrame(agd_last_step_i, columns=ds, index=ks) 
-    df_agd_last_step_i.to_csv("./data/comparison/agd"+str(sample)+".csv")
+    df_agd_last_step_i = pd.DataFrame(agd_last_step_i, columns=ds, index=ks)
+    df_agd_last_step_i.to_csv("./data/comparison/agd" + str(sample) + ".csv")
     agd_last_step += [df_agd_last_step_i]
-    df_opt_i = pd.DataFrame(opt_i, columns=ds, index=ks) 
-    df_opt_i.to_csv("./data/comparison/opt"+str(sample)+".csv")
+    df_opt_i = pd.DataFrame(opt_i, columns=ds, index=ks)
+    df_opt_i.to_csv("./data/comparison/opt" + str(sample) + ".csv")
     opt += [df_opt_i]
     print("=======================================")
     print(time.time() - t0)
     print("=======================================")
 
+import seaborn as sns
+from functools import reduce
+
 gd_last_step = []
 agd_last_step = []
 opt = []
-for i in range(N):
+for i in range(100):
     gd_last_step += [pd.read_csv("./data/comparison/gd" + str(i) + ".csv", index_col=0)]
     agd_last_step += [pd.read_csv("./data/comparison/agd" + str(i) + ".csv", index_col=0)]
     opt += [pd.read_csv("./data/comparison/opt" + str(i) + ".csv", index_col=0)]
+N = len(opt)
+agd_err = [agd_last_step[i] - opt[i] for i in range(N)]
+gd_err = [gd_last_step[i] - opt[i] for i in range(N)]
+mean_agd_err = reduce(lambda x, y: x + y, agd_err) / N
+mean_gd_err = reduce(lambda x, y: x + y, gd_err) / N
+median_agd_err = pd.DataFrame(np.zeros(mean_agd_err.shape), index=mean_agd_err.index, columns=mean_agd_err.columns)
+median_gd_err = pd.DataFrame(np.zeros(mean_gd_err.shape), index=mean_gd_err.index, columns=mean_gd_err.columns)
+for i in range(len(median_gd_err.index)):
+    for j in range(len(median_gd_err.columns)):
+        median_agd_err.iloc[i][j] = np.median(np.array([agd_err[k].iloc[i][j] for k in range(N)]))
+        median_gd_err.iloc[i][j] = np.median(np.array([gd_err[k].iloc[i][j] for k in range(N)]))
+
+median_gd_err.T.plot.bar(logy=True, ylabel='f(x) -f(x*)', xlabel='matrix dimension')
+plt.legend(loc='upper center', mode='expand', ncol=len(median_gd_err.index))
+
+median_agd_err.T.plot.bar(logy=True, ylabel='f(x) -f(x*)', xlabel='matrix dimension')
+plt.legend(loc='upper center', mode='expand', ncol=len(median_agd_err.index))
+
+ds = agd_err[0].columns
+ks = agd_err[0].index
+N = 100
+df_gd = pd.DataFrame(np.zeros((N * len(ks) * len(ds), 3)), columns=['k', 'd', 'v'])
+df_agd = pd.DataFrame(np.zeros((N * len(ks) * len(ds), 3)), columns=['k', 'd', 'v'])
+i = 0
+for j in range(N):
+    for k in ks:
+        for d in ds:
+            df_gd.iloc[i]['v'] = gd_err[j].loc[k][d]
+            df_agd.iloc[i]['v'] = agd_err[j].loc[k][d]
+            df_gd.iloc[i]['k'] = df_agd.iloc[i]['k'] = k
+            df_gd.iloc[i]['d'] = df_agd.iloc[i]['d'] = int(d)
+            i += 1
+
+matplotlib.rc('axes', titlesize=22)
+matplotlib.rc('axes', labelsize=22)
+matplotlib.rc('xtick', labelsize=22)
+matplotlib.rc('ytick', labelsize=22)
+
+figure(figsize=(12,9))
+ax = sns.boxplot(x="d", y="v", hue="k", data=df_gd)
+ax.set_yscale('log')
+ax.legend(loc='upper center', mode='expand', ncol=len(median_agd_err.index))
+ax.set_ylim(1E-11, 10)
+ax.set(xlabel='matrix dimension')
+ax.set(ylabel='f(x) - f(x*)')
+
+figure(figsize=(12,9))
+ax = sns.boxplot(x="d", y="v", hue="k", data=df_agd)
+ax.set_yscale('log')
+ax.legend(loc='upper center', mode='expand', ncol=len(median_agd_err.index))
+ax.set_ylim(1E-11, 10)
+ax.set(xlabel='matrix dimension')
+ax.set(ylabel='f(x) - f(x*)')
